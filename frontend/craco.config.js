46c5e08e3d4a5ee0a8ecfd0bb091a822e8462f20
Don Romaniello <don.romaniello@gmail.com>
2025-05-31 14:36:39 -0400

    Webpack config set.

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,13 +1,23 @@
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
+      // Add fallbacks for Node.js modules
       webpackConfig.resolve.fallback = {
         ...webpackConfig.resolve.fallback,
         buffer: require.resolve('buffer'),
         util: require.resolve('util'),
         process: require.resolve('process/browser'),
       };
+
+      // Add gray-matter-loader for .md files
+      webpackConfig.module.rules.push({
+        test: /\.md$/,
+        use: [{ loader: "gray-matter-loader" }],
+      });
+
       return webpackConfig;
     },
   },
 };
+
+
