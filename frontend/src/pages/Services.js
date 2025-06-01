155ec7e1fb9a0382b1c4cf234bf3635b6c38b2b9
Don Romaniello <don.romaniello@gmail.com>
2025-06-01 16:18:08 -0400

    People pages.

---
+++ frontend/src/pages/Services.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ContentRenderer from '../components/ContentRenderer';
+
+const Services = () => {
+  return <ContentRenderer contentFile="services" />;
+};
+
+export default Services;
